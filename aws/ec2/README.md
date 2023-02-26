595628ee8ce30e09bd718e236257a9bc27e24364
duynhanf <duynhanf@gmail.com>
2023-02-26 20:19:22 +0900

    feat: create a tf for creating an ec2 instance

---
+++ aws/ec2/README.md
@@ -0,0 +1,13 @@
+## Create an EC2 instance
+
+terraform init
+
+terraform apply
+
+## Test
+
+curl <instance name>
+
+## Destroy
+
+terraform destroy
---
+++ aws/ec2/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-1"
+}
+
+# Create an instance
+resource "aws_instance" "my-instance" {
+  ami           = "ami-0b828c1c5ac3f13ee"
+  instance_type = "t2.micro"
+
+  associate_public_ip_address = true
+  key_name                    = "nhan.dev-kp"
+
+  user_data = <<EOF
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo service nginx start
+
+EOF
+
+  tags = {
+    Name = "my-instance"
+  }
+}
